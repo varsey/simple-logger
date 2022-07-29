8f316f2a57c8b252e58c08deebbac5fdcdbdb9d4
varsey <eugeny.varseev@gmail.com>
2022-07-29 14:58:21 +0300

    ref - renamed l -> logger

--- logger-example.py
+++ logger-example.py
@@ -2,7 +2,8 @@ import datetime
 from Logger import Logger
 
 log_file_name = str(datetime.datetime.now()) + '.log'
-l = Logger(logs_filename=log_file_name)
+logger = Logger(logs_filename=log_file_name)
+
 
 if __name__ == "__main__":
-    l.log.info('Hello world!')
+    logger.log.info('Hello world!')
